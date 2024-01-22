22e3adb722e039998c03647ba3989ebc06cb4173
JoaquimVerde <joaquim.verde@gmail.com>
2024-01-22 14:37:38 +0000

    Initial commit

---
+++ src/main/java/backend/Wine_Project/WineProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.Wine_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/Wine_Project/WineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.Wine_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
