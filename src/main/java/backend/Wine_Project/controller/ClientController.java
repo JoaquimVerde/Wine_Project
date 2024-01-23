c1807ab7b33d4fbcd064376b1e5d30f208bb46f4
Claudia Lamas <claudia.c.lamas@gmail.com>
2024-01-23 14:33:37 +0000

    model Order

--- src/main/java/backend/Wine_Project/controller/ClientController.java
+++ src/main/java/backend/Wine_Project/controller/ClientController.java
@@ -1,4 +1,36 @@
 package backend.Wine_Project.controller;
 
+import backend.Wine_Project.clientDto.ClientCreateDto;
+import backend.Wine_Project.clientDto.ClientReadDto;
+import backend.Wine_Project.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/clients")
 public class ClientController {
+
+    private final ClientService clientService;
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ClientReadDto>> getClients() {
+        return new ResponseEntity<>(clientService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Long> create(@RequestBody ClientCreateDto client) {
+        return new ResponseEntity<>(clientService.create(client), HttpStatus.CREATED);
+    }
+
+
+
+
 }
