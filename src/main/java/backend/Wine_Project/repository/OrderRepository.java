feed2f1b915676a10a86b110386d7dc6b4601bc0
JoaquimVerde <joaquim.verde@gmail.com>
2024-01-24 10:39:53 +0000

    Delete Order endpoint and changed create-drop to update.

--- src/main/java/backend/Wine_Project/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.Wine_Project.repository;
-
-import backend.Wine_Project.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
