4097812a409d500b7cd7a1fbfcef9ab9352da944
JoaquimVerde <joaquim.verde@gmail.com>
2024-01-26 16:59:15 +0000

    Update wine method.

--- src/main/java/backend/Wine_Project/service/wineService/WineService.java
+++ src/main/java/backend/Wine_Project/service/wineService/WineService.java
@@ -2,6 +2,7 @@ package backend.Wine_Project.service.wineService;
 
 import backend.Wine_Project.dto.wineDto.WineCreateDto;
 import backend.Wine_Project.dto.wineDto.WineReadDto;
+import backend.Wine_Project.dto.wineDto.WineUpdateDto;
 import backend.Wine_Project.model.wine.Wine;
 import backend.Wine_Project.service.CrudService;
 
@@ -12,4 +13,5 @@ public interface WineService extends CrudService<WineReadDto, WineCreateDto, Lon
     Wine getById(Long id);
 
 
+    void updateWine(Long id, WineUpdateDto wine);
 }
