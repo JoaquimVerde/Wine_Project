4b7f84e48e80734643e3994e3e2afed636a3f9aa
andrenunes3 <152313579+andrenunes3@users.noreply.github.com>
2024-01-29 10:46:49 +0000

    Exception Handler for existing exceptions

---
+++ src/main/java/backend/Wine_Project/aspects/DivisionExceptionHandler.java
@@ -0,0 +1,32 @@
+package backend.Wine_Project.aspects;
+
+import backend.Wine_Project.exceptions.*;
+import backend.Wine_Project.util.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@Component
+@ControllerAdvice
+public class DivisionExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(DivisionExceptionHandler.class);
+
+    @ExceptionHandler(value = {ClientIdNotFoundException.class, GrapeVarietyIdNotFoundException.class, RegionIdNotFoundException.class, WineIdNotFoundException.class, WineTypeIdNotFoundException.class})
+    public ResponseEntity<String> handleIdNotFound(Exception exception){
+        logger.error(Messages.KNOWN_EXCEPTION.getMessage() + exception);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body((exception.getMessage()));
+    }
+    @ExceptionHandler(value = {EmailAlreadyExistsException.class, GrapeVarietyAlreadyExistsException.class, ItemAlreadyExistsException.class, OrderAlreadyExistsException.class, RegionAlreadyExistsException.class, WineAlreadyExistsException.class, WineTypeAlreadyExistsException.class})
+    public ResponseEntity<String> handleObjectAlreadyExists(Exception exception){
+        logger.error(Messages.KNOWN_EXCEPTION.getMessage() + exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+
+
+}
--- src/main/java/backend/Wine_Project/util/Messages.java
+++ src/main/java/backend/Wine_Project/util/Messages.java
@@ -2,23 +2,25 @@ package backend.Wine_Project.util;
 
 public enum Messages {
 
-    CLIENT_EMAIL_ALREADY_EXISTS("Client Email already exists"),
+    CLIENT_EMAIL_ALREADY_EXISTS("Client Email already exists "),
 
-    ORDER_ALREADY_EXISTS("Order Id already exists"),
+    ORDER_ALREADY_EXISTS("Order Id already exists "),
 
-    CLIENT_ID_NOT_FOUND("Client Id Not Found."),
-    WINE_ID_NOT_FOUND("Wine Id Not Found"),
-
-
-    WINE_ALREADY_EXISTS("Wine already exists!"),
-    REGION_ID_NOT_FOUND("Region Id Not Found!"),
-    GRAPE_VARIETY_ID_NOT_FOUND("Grape Variety Id Not Found!"),
-    WINE_TYPE_ID_NOT_FOUND("Wine Type Id Not Found!"),
+    CLIENT_ID_NOT_FOUND("Client Id Not Found. "),
+    WINE_ID_NOT_FOUND("Wine Id Not Found "),
 
 
-    ITEM_ALREADY_EXISTS("This item is already in shopping cart. May be you want to update the quantity!"),
+    WINE_ALREADY_EXISTS("Wine already exists! "),
+    REGION_ID_NOT_FOUND("Region Id Not Found! "),
+    GRAPE_VARIETY_ID_NOT_FOUND("Grape Variety Id Not Found! "),
+    WINE_TYPE_ID_NOT_FOUND("Wine Type Id Not Found! "),
 
-    ITEM_ID_NOT_FOUND("Item Id Not Found");
+
+    ITEM_ALREADY_EXISTS("This item is already in shopping cart. May be you want to update the quantity! "),
+
+    ITEM_ID_NOT_FOUND("Item Id Not Found! "),
+    KNOWN_EXCEPTION("Known exception: ");
+
 
 
 
