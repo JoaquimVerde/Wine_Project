7de6f6769b9cd82aae9f9ec8b7bd23d7e016add2
andrenunes3 <152313579+andrenunes3@users.noreply.github.com>
2024-02-01 12:43:32 +0000

    some grape varieties test

--- src/main/java/backend/Wine_Project/repository/GrapeVarietiesRepository.java
+++ src/main/java/backend/Wine_Project/repository/GrapeVarietiesRepository.java
@@ -1,7 +1,10 @@
 package backend.Wine_Project.repository;
 
 import backend.Wine_Project.model.wine.GrapeVarieties;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface GrapeVarietiesRepository extends JpaRepository<GrapeVarieties, Long> {
 
     Optional<GrapeVarieties> findGrapeVarietiesByName(String name);
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER TABLE grape_varieties AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
 }
