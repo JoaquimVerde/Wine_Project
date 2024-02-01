72abe90f38ab07b366c2315b8d597b3b2f2280eb
JoaquimVerde <joaquim.verde@gmail.com>
2024-02-01 13:03:52 +0000

    More tests on Wine Controller and Service

--- src/main/java/backend/Wine_Project/dto/orderDto/OrderCreateDto.java
+++ src/main/java/backend/Wine_Project/dto/orderDto/OrderCreateDto.java
@@ -3,17 +3,12 @@ package backend.Wine_Project.dto.orderDto;
 import jakarta.validation.constraints.*;
 
 public record OrderCreateDto(
-        @NotNull
-        @NotEmpty
-        @Min(value = 1)
-        Long clientId,
+
         @NotNull
         @NotEmpty
         @Min(value = 1)
         Long shoppingCartId
-        //@NotEmpty
-        //@DecimalMin("0.0")
-      //  double totalPrice
+
 
 
 ) {
