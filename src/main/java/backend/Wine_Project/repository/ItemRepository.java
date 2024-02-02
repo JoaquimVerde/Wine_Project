56cda8dabadfde632edfa9d0ad39104e67f6e4a8
JoaquimVerde <joaquim.verde@gmail.com>
2024-02-02 12:56:57 +0000

    fixed item create verification.

--- src/main/java/backend/Wine_Project/repository/ItemRepository.java
+++ src/main/java/backend/Wine_Project/repository/ItemRepository.java
@@ -1,10 +1,15 @@
 package backend.Wine_Project.repository;
 
 import backend.Wine_Project.model.Item;
+import backend.Wine_Project.model.wine.Wine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Optional<Item> findByWineAndQuantity(Wine wine, int quantity);
 
 }
