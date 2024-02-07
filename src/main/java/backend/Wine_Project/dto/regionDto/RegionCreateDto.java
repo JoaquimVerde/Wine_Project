dec8c5b73402a60ae1d057157e11420cd60945e8
JoaquimVerde <joaquim.verde@gmail.com>
2024-02-07 12:40:30 +0000

    import missing

--- src/main/java/backend/Wine_Project/dto/regionDto/RegionCreateDto.java
+++ src/main/java/backend/Wine_Project/dto/regionDto/RegionCreateDto.java
@@ -3,6 +3,7 @@ package backend.Wine_Project.dto.regionDto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record RegionCreateDto(
         @NotNull
